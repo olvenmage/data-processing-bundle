1e57cf5b225b1764c2ecc447f63fae69f545d05e
douwe gnodde <olvenmage@live.nl>
2018-10-14 21:58:35 +0200

    updated composer.json@

---
+++ OlveneerDataProcessorBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Olveneer\TwigComponentsBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OlveneerDataProcessorBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
